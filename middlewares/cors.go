80c1a3810ff21d1e6b3dbf9ad3eca24c54944e14
prachurjyaTV <prachurjya@techvariable.com>
2024-09-27 18:51:06 +0530

    added cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -7,6 +7,7 @@ func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*") // Change "*" to specific domain if necessary
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Handle preflight requests
 		if r.Method == "OPTIONS" {
